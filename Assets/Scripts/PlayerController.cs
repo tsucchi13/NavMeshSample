a61f045c0f80e0fad3671fc1e6b6610222b475bc
Tsubasa Tsuchiya <t.tsuchiya83@gmai.com>
2018-12-01 21:28:38 +0900

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public NavMeshAgent player;
+    public GameObject target;
+
+    void Start()
+    {
+        player = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            player.destination = target.transform.position;
+        }
+    }
+}
